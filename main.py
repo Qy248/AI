990465facfa531ac2dba75a1f44d52a5e93371b0
Qy248 <lowqy1918@gmail.com>
2025-04-07 00:06:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ st.set_page_config(
 # Function to load the cluster results
 def load_cluster_results():
     kmeans_df = joblib.load(r"kmeans_results.joblib") 
-    gmm_df = joblib.load(r"kmeans_results.joblib")  
+    gmm_df = joblib.load(r"gmm_clustered_rfm_data.joblib")  
     birch_df = joblib.load(r"kmeans_results.joblib") 
-    retail = joblib.load(r"kmeans_results.joblib")
+    retail = joblib.load(r"birch_results.joblib")
     
     return kmeans_df, gmm_df, birch_df, retail
 
